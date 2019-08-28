9be57942c375496b5cc6461b913ec10b39ba26f2
ae123330 <gg5201314>
2019-08-28 19:01:18 +0800

    add splice jest test

---
+++ test/splice/splice.test.js
@@ -0,0 +1,12 @@
+import splice from '../../array/splice/splice'
+
+describe('splice', () => {
+  test('splice', () => {
+    expect(splice('2,3')).toEqual(['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf'])
+  })
+
+  test('splice-args>2', () => {
+    expect(splice('2,3,4')).toEqual(['adg', 'adh', 'adi', 'aeg', 'aeh', 'aei', 'afg', 'afh', 'afi', 'bdg', 'bdh', 'bdi', 'beg', 'beh', 'bei', 'bfg', 'bfh', 'bfi', 'cdg', 'cdh', 'cdi', 'ceg', 'ceh', 'cei', 'cfg', 'cfh', 'cfi']
+    )
+  })
+})
